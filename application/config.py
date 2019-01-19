fd630818035d4da6b12b8ef8d7d5ff1365c9a0f2
AiR <ai535@ya.ru>
2019-01-19 21:34:32 +0300

    Add getModel method to controller.

---
+++ application/config.py
@@ -0,0 +1,25 @@
+from flask.json import JSONEncoder
+
+from coremltools.models.model import MLModel
+
+from core.model.Event import Event
+
+
+class CustomJSONEncoder(JSONEncoder):
+
+    def default(self, obj):
+        try:
+            if isinstance(obj, Event):
+                return obj.serialize()
+
+            if isinstance(obj, MLModel):
+                from google.protobuf.json_format import MessageToDict
+                dict_obj = MessageToDict(obj.get_spec())
+                return dict_obj
+
+            raise RuntimeError("Not defined serialization for %s %s" % (type(obj), obj))
+
+        except TypeError as ex:
+            print("Error occurred while serializing %s. %s" % (obj, ex))
+
+        return JSONEncoder.default(self, obj)
