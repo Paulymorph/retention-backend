4afddfc49cea9445706a5c0637d54b1c383d5773
artur <lukin.arthur@yandex.ru>
2019-01-31 23:17:40 +0300

    Added: db configuration

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+# Google Cloud Project ID
+PROJECT_ID = 'my-team-project-217908'
+
+# CloudSQL & SQLAlchemy configuration
+CLOUDSQL_USER = 'retentions'
+# Fill db user password
+CLOUDSQL_PASSWORD = ''
+CLOUDSQL_DATABASE = 'retention'
+CLOUDSQL_CONNECTION_NAME = 'my-team-project-217908:europe-west1:retention-db'
+
+# The CloudSQL proxy is used locally to connect to the cloudsql instance.
+# To start the proxy, use:
+#
+#   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+#
+# Port 3306 is the standard MySQL port. If you need to use a different port,
+# change the 3306 to a different port number.
+
+# Alternatively, you could use a local MySQL instance for testing.
+LOCAL_SQLALCHEMY_DATABASE_URI = (
+    'mysql+pymysql://{user}:{password}@127.0.0.1:3306/{database}').format(
+        user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
+        database=CLOUDSQL_DATABASE)
+
+# When running on App Engine a unix socket is used to connect to the cloudsql
+# instance.
+LIVE_SQLALCHEMY_DATABASE_URI = (
+    'mysql+pymysql://{user}:{password}@localhost/{database}'
+    '?unix_socket=/cloudsql/{connection_name}').format(
+        user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
+        database=CLOUDSQL_DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME)
+
+if os.environ.get('GAE_INSTANCE'):
+    SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
+else:
+    SQLALCHEMY_DATABASE_URI = LOCAL_SQLALCHEMY_DATABASE_URI
