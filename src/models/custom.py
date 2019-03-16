ea34852d5b91d0e6fbaf3e9cbd4d1bc983a5be9d
AiR <ai535@ya.ru>
2019-03-16 23:58:37 +0300

    Fix datetime serialization.

--- src/models/custom.py
+++ src/models/custom.py
@@ -1,9 +1,10 @@
+import datetime
 import logging
 
+from coremltools.models.model import MLModel
 from flask.json import JSONEncoder
 
 from src.models.event import Event
-from coremltools.models.model import MLModel
 
 
 class CustomJSONEncoder(JSONEncoder):
@@ -17,6 +18,9 @@ class CustomJSONEncoder(JSONEncoder):
                 from google.protobuf.json_format import MessageToDict
                 dict_obj = MessageToDict(obj.get_spec())
                 return dict_obj
+
+            if isinstance(obj, datetime.datetime):
+                return str(obj.strftime('%Y-%m-%d %H:%M:%S.%f'))
 
             raise RuntimeError("Not defined serialization for %s %s" % (type(obj), obj))
         except TypeError as ex:
