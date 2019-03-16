428d0e0438dc00658fcc3c03fef9ab5022894fc0
Paul Danilin <14351112+Paulymorph@users.noreply.github.com>
2019-03-16 11:21:31 +0300

    Add filtering by users from config on training Add filtering by events from config on training Add duplicated events dropping from config on training

--- src/service.py
+++ src/service.py
@@ -18,15 +18,15 @@ class ModelHolder:
             logging.debug("Start training model")
             events = self.data_provider.get_events()
             df = self.data_provider.events_to_dataframe(events)
+
             logging.info('Started DataFrame shape: {}'.format(df.shape))
-
-            # add passed events from settings['positive_event']
+            df = preparing.filter_users(df, settings=self.settings)
+            df = preparing.filter_events(df, settings=self.settings)
+            df = preparing.drop_duplicated_events(df, settings=self.settings)
             df = preparing.add_passed_event(df, settings=self.settings)
-            logging.debug('DataFrame shape after adding passed events: {}'.format(df.shape))
-
-            # add lost events from settings['negative_event']
             df = preparing.add_lost_events(df, settings=self.settings)
-            logging.debug('DataFrame shape after adding lost events: {}'.format(df.shape))
+
+            logging.debug('DataFrame shape after preprocessing: {}'.format(df.shape))
 
             model = Model(df, 'lost')
             model.fit_model()
